ffb0184f9c60db30787f1203a4af0aa440181446
Emilio Mora <emilio.mora@accenture.com>
2024-11-29 14:41:28 -0600

    - Styles   - External libraries   - Global Styles   - Default component styles (:host)   - Libraries configuration (palettes)   - Component Styles (encapsulation)     - ::ng-deep

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, MainComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, AppRoutingModule, SharedModule],
   exports: [HeaderComponent, FooterComponent, MainComponent],
 })
 export class CoreModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatButtonModule, MatIconModule],
+})
+export class SharedModule {}
